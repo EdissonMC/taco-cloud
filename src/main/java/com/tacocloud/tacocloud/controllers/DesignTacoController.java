0fd4eec79b1e6fc3b99f40b3deb441b933d823db
Angel M Correa <pattrick.adamss@gmail.com>
2023-06-26 21:27:22 -0500

    Adding the classes needed for represent the Taco model

---
+++ src/main/java/com/tacocloud/tacocloud/controllers/DesignTacoController.java
@@ -0,0 +1,13 @@
+package com.tacocloud.tacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+
+@Controller
+@SessionAttributes("tacoOrder")
+@RequestMapping("/design")
+public class DesignTacoController {
+    
+}
